b247aedb41913f00b03004a26008670110c07bf1
Hanqing Cheng <wc2206446@163.com>
2018-03-24 21:23:35 +0800

    bump version 0.1.2

--- algo.py
+++ algo.py
@@ -6,4 +6,5 @@ class Matrix:
 
     def __init__(self, row: int, column: int, default_value: float = 0):
         """
-        <method Matrix._
+        <method Matrix.__init__>
+        Initialize matrix with given s
