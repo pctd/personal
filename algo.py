70a974f3c8c5703123095d71391f8e190ad10f86
Hanqing Cheng <wc2206446@163.com>
2019-02-07 21:34:32 +0800

    mtd: nand: Allow NAND chip ids to be included standalone.

--- algo.py
+++ algo.py
@@ -209,4 +209,5 @@ class Matrix:
         Apply Sherman-Morrison formula in O(n^2).
         To learn this formula, please look this:
         https://en.wikipedia.org/wiki/Sherman%E2%80%93Morrison_formula
-        This method returns (A + uv^T)^(-1) where A^(-1
+        This method returns (A + uv^T)^(-1) where A^(-1) is self. Returns None if it's
+        im
