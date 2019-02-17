c0fc3457d10bcfa6ffefa88df05af202b76790c7
Hanqing Cheng <wc2206446@163.com>
2019-02-18 07:44:58 +0800

    Fixed error messages.

--- algo.py
+++ algo.py
@@ -234,4 +234,5 @@ class Matrix:
         assert self.row == self.column == u.row == v.row  # u, v should be column vector
         assert u.column == v.column == 1  # u, v should be column vector
 
-        # 
+        # Calculate
+        vT = v.transpos
