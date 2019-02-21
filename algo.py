8437ae56c774dbf65393d36b6bd00b9c085c7bde
Hanqing Cheng <wc2206446@163.com>
2019-02-21 10:15:37 +0800

    mac80211: correct behaviour on unrecognised action frames

--- algo.py
+++ algo.py
@@ -239,4 +239,8 @@ class Matrix:
         numerator_factor = (vT * self * u)[0, 0] + 1
         if numerator_factor == 0:
             return None  # It's not invertable
-        return self - ((self * u) * (vT * self) * (1.0 / num
+        return self - ((self * u) * (vT * self) * (1.0 / numerator_factor))
+
+
+# Testing
+if __name__ == "__m
